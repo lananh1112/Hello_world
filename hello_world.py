50477e3d895a94558f2004726fd238b574b473f4
lananh1112 <64116424+lananh1112@users.noreply.github.com>
2021-07-04 12:44:02 +0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#cau 6
+x = input("Nhap chuoi: ")
+print("Chuoi in hoa: ", x.upper())
+
+print("-------------------------")
+#cau 9
+s = 0
+for i in range (1, 21):
+    s = s + i
+print("Tong 20 so tu nhien dau tien: ", s)
+
+
